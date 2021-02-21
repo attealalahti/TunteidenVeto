f75d13ae763654f080c43992a3ad878bf8d1bb4c
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2021-02-21 12:52:17 +0200

    The screen is drawn based on currentScreen.

--- Game/core/src/fi/tuni/tiko/MainGame.java
+++ Game/core/src/fi/tuni/tiko/MainGame.java
@@ -30,6 +30,8 @@ public class MainGame extends ApplicationAdapter {
 	ChoiceScreen screen2a;
 	ChoiceScreen screen2b;
 
+	ChoiceScreen currentScreen;
+
 	Skin skin;
 	@Override
 	public void create () {
@@ -43,13 +45,10 @@ public class MainGame extends ApplicationAdapter {
 		answers = new ArrayList<String>();
 		answers.add(" ...tunnet itsesi hölmöksi. \n Oletkohan tehnyt jotakin, \n mistä hän on suuttunut?");
 		answers.add(" ...alat kysellä, mikä vaivaa. \n Hän ei selvästikään ole oma itsensä.");
-		//answers.add("Another option.");
 		ArrayList<Integer> screenLinks = new ArrayList<Integer>();
 		screenLinks.add(1);
 		screenLinks.add(2);
-
 		screen = new ChoiceScreen(0, skin, question, answers, screenLinks);
-		Gdx.input.setInputProcessor(screen);
 
 		String feedback2a = "Tilanne jää mietityttämään. \n Oliko ystäväsi kuitenkin \n vihaisen sijaan surullinen? \n Et oikein tiedä, \n mitä tilanteesta pitäisi ajatella.";
 		ArrayList<String> answers2a = new ArrayList<String>();
@@ -60,10 +59,15 @@ public class MainGame extends ApplicationAdapter {
 
 		String feedback2b = "Hetken päästä ystäväsi kertoo,\n että hänen lemmikkinsä kuoli \n muutama päivä sitten. \n Juttelette tilanteesta hetken ja \n ystäväsi vaikuttaa tulevan paremmille mielin. \n Itseäsi alkaa hiukan surettaa ystäväsi puolesta, \n mutta olet iloinen että juttelusta \n tuli hänelle parempi mieli.";
 		ArrayList<String> answers2b = new ArrayList<String>();
-		answers2a.add("SURU +5, ILO +15");
+		answers2b.add("SURU +5, ILO +15");
+		answers2b.add("another option");
 		ArrayList<Integer> screenLinks3 = new ArrayList<Integer>();
 		screenLinks3.add(0);
+		screenLinks3.add(1);
 		screen2b = new ChoiceScreen(2, skin, feedback2b, answers2b, screenLinks3);
+
+		currentScreen = screen2b;
+		Gdx.input.setInputProcessor(currentScreen);
 	}
 	public Skin createSkin() {
 		Skin s = new Skin();
@@ -91,8 +95,8 @@ public class MainGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 0.1f, 0.1f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		screen.draw();
-		screen.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 60f));
+		currentScreen.draw();
+		currentScreen.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 60f));
 	}
 
 	@Override
