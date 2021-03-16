be4eae4c8a4d52f10895ca6bc1c3c0aae6f7a6b7
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2021-03-16 21:18:53 +0200

    Corrected documentation for Screen.

--- Game/core/src/fi/tuni/tiko/Screen.java
+++ Game/core/src/fi/tuni/tiko/Screen.java
@@ -5,6 +5,10 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 import java.util.ArrayList;
 
+/** Screen is an abstract class that is used to create different screen layouts for the game.
+ * Every screen has a screen ID and some choices that can move the game to a different screen.
+ * @author Atte Ala-Lahti
+ */
 abstract public class Screen extends Stage {
 
     private int screenID;
@@ -13,14 +17,12 @@ abstract public class Screen extends Stage {
     private ArrayList<Integer> screenLinks;
     int nextScreensID = 0;
 
-    /** Screen is an abstract class that is used to create different screen layouts for the game.
-     * Every screen has a screen ID and some choices that moves the game to a different screen.
+    /** Creates a new screen
      *
      * @param screenID a unique integer used in MainGame to determine which screen to show
      * @param skin contains styles for all objects
      * @param choices text for each of the choices
      * @param screenLinks screen IDs for screens the choices lead to. Must be the same size as choices
-     * @author Atte Ala-Lahti
      */
     public Screen (int screenID, Skin skin, ArrayList<String> choices, ArrayList<Integer> screenLinks) {
         this.screenID = screenID;
@@ -41,7 +43,7 @@ abstract public class Screen extends Stage {
     public ArrayList<Integer> getScreenLinks() {
         return screenLinks;
     }
-    /** Happens when one of the answers is selected and confirmed.
+    /** Called by an AnswerBox when it is selected and confirmed.
      *
      * @param screenLink The ID number of the next screen.
      */
