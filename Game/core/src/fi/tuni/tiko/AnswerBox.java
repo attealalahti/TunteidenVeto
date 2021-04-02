e3f13d8b32e0d02b5c8eb287ffd9ddab09082ad7
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2021-04-02 11:52:18 +0300

    Class added.

---
+++ Game/core/src/fi/tuni/tiko/AnswerBox.java
@@ -0,0 +1,53 @@
+package fi.tuni.tiko;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.moveTo;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.run;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.sequence;
+import static fi.tuni.tiko.MainGame.skin;
+import static fi.tuni.tiko.MainGame.windowHeight;
+import static fi.tuni.tiko.MainGame.windowWidth;
+
+
+public class AnswerBox extends Group {
+    private Label background;
+    private Label textBox;
+    private float textMargin = 0.14f;
+    /**
+     * Creates a new AnswerBox.
+     * An AnswerBox is comprised of a background label and a text box label to precisely control where the text can be.
+     * In the future, different backgrounds might require different text box sizes.
+     *
+     * @param text       the text in the text box
+     * @param x          horizontal coordinate in pixels
+     * @param y          vertical coordinate in pixels
+     * @param width      width of the background in pixels
+     * @param height     height of the background in pixels
+     */
+    public AnswerBox(CharSequence text, float x, float y, float width, float height) {
+        background = new Label(null, skin, "answer_static");
+        textBox = new Label(text, skin, "text");
+        background.setBounds(x, y, width, height);
+
+        // Calculating the dimensions of the text box
+        float xMargin = textMargin * width;
+        float yMargin = textMargin * height;
+        textBox.setBounds(x + xMargin * 0.5f, y + yMargin * 0.5f, width - xMargin, height - yMargin);
+        textBox.setFontScaleX(0.000324f * windowWidth);
+        textBox.setFontScaleY(0.00018f * windowHeight);
+        textBox.setWrap(true);
+        textBox.setAlignment(0);
+
+        addActor(background);
+        addActor(textBox);
+    }
+    public Label getBackground() {
+        return background;
+    }
+}
