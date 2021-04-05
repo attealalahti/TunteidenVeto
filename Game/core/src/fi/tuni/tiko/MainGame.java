1bc0f9873350584b5653434991c63bb4eac38f31
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2021-04-05 23:56:20 +0300

    Plot fixes.

--- Game/core/src/fi/tuni/tiko/MainGame.java
+++ Game/core/src/fi/tuni/tiko/MainGame.java
@@ -189,7 +189,7 @@ public class MainGame extends ApplicationAdapter {
 		currentScreen.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 60f));
 	}
 	public ArrayList<ChoiceScreen> createChoiceScreens() {
-		FileHandle handle = Gdx.files.internal("test2leveldata.feel");
+		FileHandle handle = Gdx.files.internal("fullleveldata.txt");
 		String text = handle.readString();
 		String [] allLines = text.split("\\r?\\n");
 
